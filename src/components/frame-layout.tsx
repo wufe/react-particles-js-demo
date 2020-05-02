7183c8ced0136d32ea7019e7c03fdae1eb347dd9
Simone Bembi <simone.bembi@gmail.com>
2020-05-02 12:03:29 +0200

    Using react-particles-js v3.2.1

--- src/components/frame-layout.tsx
+++ src/components/frame-layout.tsx
@@ -108,7 +108,7 @@ export class FrameLayout extends React.Component<IProps, IState> {
                         className="github-mark"
                         href="https://github.com/Wufe/react-particles-js"
                         target="_blank"></a>
-                    <span>v3.2.0</span>
+                    <span>v3.2.1</span>
                 </div>
 
             </div>
--- src/components/particles-container.tsx
+++ src/components/particles-container.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import Particles, { IParticlesParams } from 'react-particles-js';
+import { createRef } from 'react';
 
 interface IProps {
     params: IParticlesParams;
 }
 
 export class ParticlesContainer extends React.PureComponent<IProps> {
+
+    ref = createRef<any>();
+
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log('Particles ref:', this.ref);
+    }
+
     render() {
-        return <Particles params={this.props.params} className="frame-layout__particles"/>
+        return <Particles particlesRef={this.ref} params={this.props.params} className="frame-layout__particles"/>
     }
 }
